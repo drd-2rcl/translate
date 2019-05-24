863a723720bbde95948bcdc0c0c7d2472a172930
drd-2rcl <drd.rclrd@gmail.com>
2019-05-23 22:10:03 -0300

    was set App for selecting a language

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
   render() {
-    return <div>App</div>
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag br" onClick={() => this.onLanguageChange('português-br')} />
+          <i className="flag jm" onClick={() => this.onLanguageChange('english')} />
+        </div>
+        {this.state.language}
+      </div>
+    )
   }
 }
 
