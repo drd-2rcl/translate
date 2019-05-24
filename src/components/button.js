2ffb7eff644d7c08392ee742daa58a904001ac13
drd-2rcl <drd.rclrd@gmail.com>
2019-05-23 22:40:16 -0300

    was imported context API in component button

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
+import LanguageContext from '../contexts/language_context';
+
 class Button extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }
