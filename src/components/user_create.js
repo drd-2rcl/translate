763d0d5917149383e116ba1e67db0283248f2b24
drd-2rcl <drd.rclrd@gmail.com>
2019-05-23 22:23:37 -0300

    was created component user create

---
+++ src/components/user_create.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Button from './button';
+import Field from './field';
+
+const UserCreate = () => {
+  return (
+    <div className="ui form">
+      <Field />
+      <Button />
+    </div>
+  )
+}
+
+export default UserCreate;
