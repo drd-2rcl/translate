49eea4d72d8aaa81f4988ebbefd60e1bd22f5032
drd-2rcl <drd.rclrd@gmail.com>
2019-05-25 18:01:05 -0300

    was inseted context to name word

--- src/components/field.js
+++ src/components/field.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import LanguageContext from '../contexts/language_context';
 
 class Field extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'english' ? 'Name' : 'Nome';
+
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     )
