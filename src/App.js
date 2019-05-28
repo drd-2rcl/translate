df7a8dbc4ca580cadb2af7cd9185a6ef4eb4de1d
drd-2rcl <drd.rclrd@gmail.com>
2019-05-27 22:21:54 -0300

    was refatored for receive language store

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
 
 import LanguageSelector from './components/language_selector';
-import LanguageContext from './contexts/language_context';
+import { LanguageStore } from './contexts/language_context';
 import ColorContext from './contexts/color_context';
 import UserCreate from './components/user_create'
 
 class App extends Component {
-  state = { language: 'english' };
-
-  onLanguageChange = language => {
-    this.setState({ language })
-  }
-
   render() {
-    const { language } = this.state;
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+        <LanguageStore>
+          <LanguageSelector onLanguageChange={this.onLanguageChange} />
 
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={language}>
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider> 
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     )
   }
