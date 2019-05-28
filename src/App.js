ad1bcc52867b26a7775fdcedda673642f65aeb30
drd-2rcl <drd.rclrd@gmail.com>
2019-05-27 21:47:23 -0300

    was inserted the selector language component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
-import UserCreate from './components/user_create'
+import LanguageSelector from './components/language_selector';
 import LanguageContext from './contexts/language_context';
 import ColorContext from './contexts/color_context';
+import UserCreate from './components/user_create'
 
 class App extends Component {
   state = { language: 'english' };
@@ -15,11 +16,8 @@ class App extends Component {
     const { language } = this.state;
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i className="flag jm" onClick={() => this.onLanguageChange('english')} />
-          <i className="flag br" onClick={() => this.onLanguageChange('português-br')} />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+
         <ColorContext.Provider value="red">
           <LanguageContext.Provider value={language}>
             <UserCreate />
